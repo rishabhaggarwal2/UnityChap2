ec5a3b5d6146685d23daa3bf8a865d638c233a1f
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-01-18 16:33:00 -0800

    First commit

---
+++ Assets/DestroyablePillar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyablePillar : MonoBehaviour {
+
+	public void ReactToHit() {
+		StartCoroutine (Die ());
+	}
+
+	private IEnumerator Die() {
+		this.transform.Translate (0, -8, 0);
+
+		yield return new WaitForSeconds (1.5f);
+
+		Destroy (this.gameObject);
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Unity Chapter 2
